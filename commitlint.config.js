e554917aa04609ec9288234e7b51e6e23366468f
MorarMarian <marian.morar@flutteruki.com>
2024-09-23 12:52:03 +0300

    feat(TEAM-1234): add login stuff

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,16 @@
 module.exports = {
     rules: {
+      // Define allowed commit types
       'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
-      'subject-pattern': [2, 'always', '^TEAM-[0-9]{1,5}:\\s.{1,50}$'],
-      'subject-case': [0],  // Disable default case enforcement
-    },
+      
+      // Ensure the commit message matches "<type>(TEAM-<jira-ticket-number>): <description>"
+      'subject-pattern': [2, 'always', '^([a-z]+)\\(TEAM-[0-9]+\\):\\s.{1,50}$'],
+      
+      // Ignore subject case rules, as this might be causing conflicts
+      'subject-case': [0],
+  
+      // Optional: Limit subject length to 50 characters
+      'subject-max-length': [2, 'always', 72],
+    }
   };
   
